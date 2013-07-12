bd8a881cf6d733bea59f39454c6dd2cada988a9e
xufei <xu.fei@outlook.com>
2013-07-12 16:57:33 +0800

    update document

--- js/modules/core/promise.js
+++ js/modules/core/promise.js
@@ -1,199 +1,133 @@
 /**
  * Promise
  */
-thin.module("Promise", [], function () {
+thin.define("Promise", ["PromiseState"], function () {
+	function isPromise() {
 
-	var defer = function () {
-		var pending = [];
-		var value;
+	}
 
-		var deferred = {
-			resolve: function (val) {
-				if (pending) {
-					var callbacks = pending;
-					pending = undefined;
-					value = ref(val);
+	var PromiseState = {
+		PENDING: 0,
+		FULFILLED: 1,
+		REJECTED: 2
+	};
 
-					if (callbacks.length) {
-						callLater(function () {
-							var callback;
-							for (var i = 0, ii = callbacks.length; i < ii; i++) {
-								callback = callbacks[i];
-								value.then(callback[0], callback[1]);
-							}
-						});
-					}
+	function Promise() {
+		this._resolves = [];
+		this._rejects = [];
+		this._readyState = PromiseState.PENDING;
+		this._data = null;
+		this._reason = null;
+	}
+
+	Promise.prototype = {
+		then: function (onFulfilled, onRejected) {
+			var deferred = new Defer();
+
+			function fulfill(data) {
+				var ret = onFulfilled ? onFulfilled(data) : data;
+				if (isPromise(ret)) {
+					ret.then(function (data) {
+						deferred.resolve(data);
+					});
+				} else {
+					deferred.resolve(ret);
 				}
-			},
+				return ret;
+			}
 
-			reject: function (reason) {
-				deferred.resolve(reject(reason));
-			},
+			if (this._readyState === PromiseState.PENDING) {
+				this._resolves.push(fulfill);
 
-			promise: {
-				then: function (callback, errback) {
-					var result = defer();
-
-					var wrappedCallback = function (value) {
-						try {
-							result.resolve((callback || defaultCallback)(value));
-						} catch (e) {
-							exceptionHandler(e);
-							result.reject(e);
-						}
-					};
-
-					var wrappedErrback = function (reason) {
-						try {
-							result.resolve((errback || defaultErrback)(reason));
-						} catch (e) {
-							exceptionHandler(e);
-							result.reject(e);
-						}
-					};
-
-					if (pending) {
-						pending.push([wrappedCallback, wrappedErrback]);
-					} else {
-						value.then(wrappedCallback, wrappedErrback);
-					}
-
-					return result.promise;
-				},
-
-				always: function (callback) {
-					function makePromise(value, resolved) {
-						var result = defer();
-						if (resolved) {
-							result.resolve(value);
-						} else {
-							result.reject(value);
-						}
-						return result.promise;
-					}
-
-					function handleCallback(value, isResolved) {
-						var callbackOutput = null;
-						try {
-							callbackOutput = (callback || defaultCallback)();
-						} catch (e) {
-							return makePromise(e, false);
-						}
-						if (callbackOutput && callbackOutput.then) {
-							return callbackOutput.then(function () {
-								return makePromise(value, isResolved);
-							}, function (error) {
-								return makePromise(error, false);
-							});
-						} else {
-							return makePromise(value, isResolved);
-						}
-					}
-
-					return this.then(function (value) {
-						return handleCallback(value, true);
-					}, function (error) {
-						return handleCallback(error, false);
+				if (onRejected) {
+					this._rejects.push(onRejected);
+				} else {
+					//为了让reject向后传递
+					this._rejects.push(function (reason) {
+						deferred.reject(reason);
 					});
 				}
-			}
-		};
-
-		return deferred;
-	};
-
-	var ref = function (value) {
-		if (value && value.then) return value;
-		return {
-			then: function (callback) {
-				var result = defer();
-				nextTick(function () {
-					result.resolve(callback(value));
+			} else if (this._readyState === PromiseState.FULFILLED) {
+				var self = this;
+				setTimeout(function () {
+					fulfill(self._data);
 				});
-				return result.promise;
 			}
-		};
-	};
+			return deferred.promise;
+		},
 
-	var reject = function (reason) {
-		return {
-			then: function (callback, errback) {
-				var result = defer();
-				nextTick(function () {
-					result.resolve((errback || defaultErrback)(reason));
-				});
-				return result.promise;
-			}
-		};
-	};
-
-	var when = function (value, callback, errback) {
-		var result = defer(),
-			done;
-
-		var wrappedCallback = function (value) {
-			try {
-				return (callback || defaultCallback)(value);
-			} catch (e) {
-				exceptionHandler(e);
-				return reject(e);
-			}
-		};
-
-		var wrappedErrback = function (reason) {
-			try {
-				return (errback || defaultErrback)(reason);
-			} catch (e) {
-				exceptionHandler(e);
-				return reject(e);
-			}
-		};
-
-		callLater(function () {
-			ref(value).then(function (value) {
-				if (done) return;
-				done = true;
-				result.resolve(ref(value).then(wrappedCallback, wrappedErrback));
-			}, function (reason) {
-				if (done) return;
-				done = true;
-				result.resolve(wrappedErrback(reason));
-			});
-		});
-
-		return result.promise;
-	};
-
-	function defaultCallback(value) {
-		return value;
-	}
-
-	function defaultErrback(reason) {
-		return reject(reason);
-	}
-
-	function all(promises) {
-		var deferred = defer(),
-			counter = 0,
-			results = isArray(promises) ? [] : {};
-
-		forEach(promises, function (promise, key) {
-			counter++;
-			ref(promise).then(function (value) {
-				if (results.hasOwnProperty(key)) return;
-				results[key] = value;
-				if (!(--counter)) deferred.resolve(results);
-			}, function (reason) {
-				if (results.hasOwnProperty(key)) return;
-				deferred.reject(reason);
-			});
-		});
-
-		if (counter === 0) {
-			deferred.resolve(results);
+		otherwise: function (onRejected) {
+			return this.then(undefined, onRejected);
 		}
+	};
 
-		return deferred.promise;
+	function Defer() {
+		this.promise = new Promise();
 	}
 
+	Defer.prototype = {
+		resolve: function (data) {
+			var promise = this.promise;
+			if (promise._readyState != PromiseState.PENDING) {
+				return;
+			}
+
+			promise._readyState = PromiseState.FULFILLED;
+			promise._data = data;
+
+			promise._resolves.forEach(function (handler) {
+				handler(data);
+			});
+		},
+
+		reject: function (reason) {
+			var promise = this.promise;
+			if (promise._readyState != PromiseState.PENDING) {
+				return;
+			}
+			promise._readyState = PromiseState.REJECTED;
+			promise._reason = reason;
+
+			var handler = promise._rejects[0];
+			if (handler) {
+				handler(reason);
+			}
+		}
+	};
+
+	return {
+		defer: function(){
+			return new Defer();
+		},
+
+		all: function(promises){
+			var deferred = Defer();
+
+			var n = 0, result = [];
+			promises.forEach(function(promise){
+				promise.then(function(ret){
+					result.push(ret);
+					n++;
+
+					if(n >= promises.length){
+						deferred.resolve(result);
+					}
+				});
+			});
+
+			return deferred.promise;
+		},
+
+		any: function(promises){
+			var deferred = Defer();
+
+			promises.forEach(function(promise){
+				promise.then(function(ret){
+					deferred.resolve(ret);
+				});
+			});
+
+			return deferred.promise;
+		}
+	};
 });
