37298e815cffbf3c11f2d9d2995fa38fbf6d3a28
xufei <xu.fei@outlook.com>
2014-05-05 17:37:10 +0800

    以后再写吧

---
+++ js/modules/controls/scrollbar.js
@@ -0,0 +1,11 @@
+thin.define("ScrollBar", ["_", "Events"], function (_, Events) {
+	var ScrollBar = function(element, config) {
+		element.innerHTML = "";
+	};
+
+	ScrollBar.prototype = {
+
+	};
+
+	return ScrollBar;
+}；
